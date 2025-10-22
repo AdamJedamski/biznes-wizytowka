5262c96478c65d4052c7eb727842776a34827071
AdamJedamski <Adam.Jedamski@gmail.com>
2025-10-22 18:51:46 +0200

    Initial static export of Kontenery Premium2

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// Simple client-side form handler (no backend)
-document.getElementById('contactForm').addEventListener('submit', function(e){
-  e.preventDefault();
-  alert('Dziękujemy! Twoja wiadomość została przesłana (symulacja).');
-  this.reset();
-});
+console.log('Hello world');
